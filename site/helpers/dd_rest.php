33546b5bc5f077d319b55e30d7d851e99bc49157
Florian Häusler <florian@didldu.com>
2017-04-24 13:38:01 +0200

    Added helper class DD_RestHelper

---
+++ site/helpers/dd_rest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @version    1-1-0-1 // Y-m-d 2017-04-24
+ * @author     HR IT-Solutions Florian Häusler https://www.hr-it-solutions.com
+ * @copyright  Copyright (C) 2011 - 2017 Didldu e.K. | HR IT-Solutions
+ * @license    http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only
+ **/
+
+defined('_JEXEC') or die;
+
+/**
+ * Class DD_RestHelper
+ *
+ * @since  Version 1.1.0.0
+ */
+class DD_RestHelper
+{
+
+}
