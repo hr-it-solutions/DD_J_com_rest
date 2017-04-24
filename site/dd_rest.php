d1f7196aae3e006d0ae2af2e72ad47b53c4509bc
Florian Häusler <florian@didldu.com>
2017-04-24 12:52:30 +0200

    Entrie point added

---
+++ site/dd_rest.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @version    1-1-0-0 // Y-m-d 2017-04-24
+ * @author     HR IT-Solutions Florian Häusler https://www.hr-it-solutions.com
+ * @copyright  Copyright (C) 2011 - 2017 Didldu e.K. | HR IT-Solutions
+ * @license    http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only
+ **/
+
+defined('_JEXEC') or die;
+
+$controller = JControllerLegacy::getInstance('DD_Rest');
+$controller->execute(JFactory::getApplication()->input->get('task'));
+$controller->redirect();
