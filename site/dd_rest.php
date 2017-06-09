ca550c7dbd6c68c78dbd23d2e5c3e9fc65e3c641
Florian Häusler <florian@didldu.com>
2017-06-10 00:51:42 +0200

    api

--- site/dd_rest.php
+++ site/dd_rest.php
@@ -11,5 +11,5 @@ defined('_JEXEC') or die;
 require_once JPATH_COMPONENT . '/helpers/dd_rest.php';
 
 $controller = JControllerLegacy::getInstance('DD_Rest');
-$controller->execute(JFactory::getApplication()->input->get('task'));
+$controller->execute(JFactory::getApplication()->input->get('api'));
 $controller->redirect();
